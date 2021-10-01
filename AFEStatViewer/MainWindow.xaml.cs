2a432f441aec47a7cc9ec3eac51c18f51541f241
Jason Hutton <4064494+JasonHutton@users.noreply.github.com>
2021-10-01 00:18:30 -0700

    Move NAryDictionary stuff into its own file.

--- AFEStatViewer/MainWindow.xaml.cs
+++ AFEStatViewer/MainWindow.xaml.cs
@@ -19,35 +19,6 @@ using System.Diagnostics;
 
 namespace AFEStatViewer
 {
-    // https://softwareengineering.stackexchange.com/questions/319264/dictionary-of-dictionaries-design-in-c
-    public class NAryDictionary<TKey, TValue> :
-    Dictionary<TKey, TValue>
-    {
-    }
-
-    public class NAryDictionary<TKey1, TKey2, TValue> :
-    Dictionary<TKey1, NAryDictionary<TKey2, TValue>>
-    {
-    }
-
-    public class NAryDictionary<TKey1, TKey2, TKey3, TValue> :
-    Dictionary<TKey1, NAryDictionary<TKey2, TKey3, TValue>>
-    {
-    }
-
-    public static class NAryDictionaryExtensions
-    {
-        public static NAryDictionary<TKey2, TValue> New<TKey1, TKey2, TValue>(this NAryDictionary<TKey1, TKey2, TValue> dictionary)
-        {
-            return new NAryDictionary<TKey2, TValue>();
-        }
-
-        public static NAryDictionary<TKey2, TKey3, TValue> New<TKey1, TKey2, TKey3, TValue>(this NAryDictionary<TKey1, TKey2, TKey3, TValue> dictionary)
-        {
-            return new NAryDictionary<TKey2, TKey3, TValue>();
-        }
-    }
-
     public class CampaignCompletion
     {
         public CampaignCompletion()
